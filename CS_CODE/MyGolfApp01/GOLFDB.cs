529e96078c4bd071b61623c9f55b08f3f5cae2af
KEUM HONG LIM <kiwizen.code@hotmail.com>
2017-04-08 21:52:42 +1200

    change ListBox to ListView. finally able to display somthing on ListView

---
+++ CS_CODE/MyGolfApp01/GOLFDB.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace MyGolfApp01
+{
+    class GOLFDB
+    {
+        private static string ConnectionString = @"Server=tcp:kiwizen-dsed.database.windows.net,1433;Initial Catalog=DSED_GOLF;Persist Security Info=False;User ID=kiwizen-dsed;Password=Monday99;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+   
+        public static string ID { get => "ID"; }
+        public static string Title { get => "Title"; }
+        public static string SurName { get => "Surname"; }
+        public static string FirstName { get => "Firstname"; }
+        public static string Gender { get => "Gender"; }
+        public static string DOB { get => "DOB";  }
+        public static string Street { get => "Street"; }
+        public static string Suburb { get => "Suburd"; }
+        public static string City { get => "City"; }
+        public static string Availiable_Week_Days { get => "Available week days"; }
+        public static string Handicap { get => "Handicap"; }
+
+        string[] db_feilds = new string[] {
+                "ID", "Surname", "Firstname", "Title",
+                "Gender", "DOB","Street","Suburb", "City",
+                "Available week days", "Handicap"
+            };
+
+        public delegate void ProcessRecordHandler(SqlDataReader record);
+
+        public static void ProcessRecord(ProcessRecordHandler recordHandler)
+        {
+            string SelectAllQuery = 
+                "SELECT [ID],[Surname],[Firstname],[Title],[Gender],[DOB],[Street]" +
+                ",[Suburb],[City],[Available week days],[Handicap]" +
+                "FROM[dbo].[golf]";
+
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand(SelectAllQuery,connection))
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        Dictionary<string, string> dict_Record =
+                                        new Dictionary<string, string>();
+                        while (reader.Read())
+                        {
+                            if (recordHandler != null)
+                                recordHandler(reader);
+                        }
+                    }
+                    connection.Close();
+                }
+                catch( Exception e)
+                { }
+                finally
+                {
+                    connection.Close();
+                }
+                
+            }
+        }
+
+    }
+}
--- CS_CODE/MyGolfApp01/MainWindow.xaml.cs
+++ CS_CODE/MyGolfApp01/MainWindow.xaml.cs
@@ -20,10 +20,29 @@ namespace MyGolfApp01
     /// </summary>
     public partial class MainWindow : Window
     {
+        GOLFDB.ProcessRecordHandler myDataHandler = null;
+
         public MainWindow()
         {
             InitializeComponent();
             WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen;
+            myDataHandler = this.InitializeListBox_PlayerList;
+            GOLFDB.ProcessRecord(myDataHandler);
+            
+        }
+
+        private void InitializeListBox_PlayerList(System.Data.SqlClient.SqlDataReader record)
+        {
+            System.Globalization.CultureInfo culture = new System.Globalization.CultureInfo("fr-FR");
+            this.ListBox_PlayerList.Items.Add(new {
+                ID_Col = record[GOLFDB.ID].ToString(),
+                Title_Col = record[GOLFDB.Title].ToString(),
+                FName_Col = record[GOLFDB.FirstName].ToString(),
+                LName_Col = record[GOLFDB.SurName].ToString(),
+                DOB_Col = record.GetDateTime(record.GetOrdinal(GOLFDB.DOB)).ToString("d", culture)
+
+                
+            });
         }
     }
 }
